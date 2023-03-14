ac8e1d41cdefc656b794656566a2a8dbb4842677
branchwelder <htwiggsmith@gmail.com>
2023-03-13 19:29:58 -0700

    snippets 5ever

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # patterns
+
+- [x] split pane editors
+- [x] resizable panes
+- [x] iframe result
+- [x] update iframe on type
+- [x] save snippet
+- [x] snippet dropdown
+- [x] copy button
+- [ ] output console
+- [ ] double click editor panel title to expand/collapse
+- [ ] save editor state with snippet
--- index.js
+++ index.js
@@ -1,45 +1,208 @@
 import { EditorView } from "@codemirror/view";
+import { EditorState } from "@codemirror/state";
+import Split from "split.js";
+import { html, render } from "lit-html";
 
 import { css } from "@codemirror/lang-css";
-import { html } from "@codemirror/lang-html";
+import { html as langHTML } from "@codemirror/lang-html";
 import { javascript } from "@codemirror/lang-javascript";
 
 import { editorSetup } from "./editor";
-import { resizeHandler } from "./resize.js";
+import snippets from "./snippets.json";
 
-const workspace = document.getElementById("workspace");
+let htmlView, cssView, jsView;
 
-const edHTML = document.getElementById("editor-html");
-const edCSS = document.getElementById("editor-css");
-const edJS = document.getElementById("editor-js");
+let snippet = snippets["downloadJSON"];
 
-let cssView = new EditorView({
-  doc: `p, h1, h2, h3, h4, h5, h6 {
-  overflow-wrap: break-word;
-}`,
-  parent: edCSS,
-  extensions: [editorSetup, css()],
+function copyCode(e, lang) {
+  navigator.clipboard.writeText(snippet[lang]);
+
+  e.target.classList.add("success");
+
+  setTimeout(() => {
+    e.target.classList.remove("success");
+  }, 1);
+}
+
+function saveSnippet() {
+  const downloadLink = document.createElement("a");
+  downloadLink.href =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(JSON.stringify(snippet));
+  downloadLink.download = "snippet.json";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+function listener(lang) {
+  return EditorView.updateListener.of((v) => {
+    snippet[lang] = v.state.doc.toString();
+    renderView();
+  });
+}
+
+function setEditorStates() {
+  cssView.setState(cssState(snippet.css));
+  htmlView.setState(htmlState(snippet.html));
+  jsView.setState(jsState(snippet.js));
+}
+
+function loadSnippet(e) {
+  let snippetID = e.target.dataset.snippetid;
+  snippet = snippets[snippetID];
+
+  setEditorStates();
+  renderView();
+}
+
+function cssState(stateDoc) {
+  return EditorState.create({
+    doc: stateDoc,
+    extensions: [editorSetup, css(), listener("css")],
+  });
+}
+
+function htmlState(stateDoc) {
+  return EditorState.create({
+    doc: stateDoc,
+    extensions: [editorSetup, langHTML(), listener("html")],
+  });
+}
+
+function jsState(stateDoc) {
+  return EditorState.create({
+    doc: stateDoc,
+    extensions: [editorSetup, javascript(), listener("js")],
+  });
+}
+
+function makeSrc() {
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Output</title>
+        <style>
+
+          ${snippet.css}
+        </style>
+      </head>
+      <body>
+        ${snippet.html}
+        <script>
+          const _log = console.log;
+          console.log = function(...rest) {
+            window.parent.postMessage(
+              {
+                source: 'iframe',
+                message: rest,
+              },
+              '*'
+            );
+            _log.apply(console, arguments);
+          };
+          ${snippet.js};
+        </script>
+      </body>
+    </html>`;
+}
+
+window.addEventListener("message", function (response) {
+  if (response.data && response.data.source === "iframe") {
+    console.log("from output", response.data.message);
+  }
 });
 
-let htmlView = new EditorView({
-  doc: `<div id="editor-container">
-  <div id="editor-html"></div>
-  <div id="editor-css"></div>
-  <div id="editor-js"></div>
-</div>
-<div id="output"></div>`,
-  parent: edHTML,
-  extensions: [editorSetup, html()],
-});
+function view() {
+  return html` <div id="editor-container">
+      <div id="toolbar">
+        <span>snippets</span>
+        <span id="icons">
+          <i @click=${saveSnippet} class="fa-solid fa-floppy-disk fa-fw"></i>
+          <i
+            id="pattern-icon"
+            @click=${loadSnippet}
+            class="fa-solid fa-book fa-fw">
+            <div id="pattern-dropdown">
+              ${Object.keys(snippets).map(
+                (snippetID) =>
+                  html`<div data-snippetid=${snippetID}>${snippetID}</div>`
+              )}
+            </div>
+          </i>
+        </span>
+      </div>
+      <div id="editor-html" class="editor" style="--color: var(--orange)">
+        <div class="editor-title">
+          <span>html</span>
+          <i
+            @click=${(e) => copyCode(e, "html")}
+            class="fa-solid fa-copy fa-xs">
+            <div class="copied">copied!</div>
+          </i>
+        </div>
+        <div id="html-container"></div>
+      </div>
+      <div id="editor-css" class="editor" style="--color: var(--green)">
+        <div class="editor-title">
+          <span>css</span>
+          <i @click=${(e) => copyCode(e, "css")} class="fa-solid fa-copy fa-xs">
+            <div class="copied">copied!</div>
+          </i>
+        </div>
+        <div id="css-container"></div>
+      </div>
+      <div id="editor-js" class="editor" style="--color: var(--purple)">
+        <div class="editor-title">
+          <span>javascript</span>
+          <i @click=${(e) => copyCode(e, "js")} class="fa-solid fa-copy fa-xs">
+            <div class="copied">copied!</div>
+          </i>
+        </div>
+        <div id="js-container"></div>
+      </div>
+    </div>
+    <div id="output">
+      <iframe srcdoc=${makeSrc()}></iframe>
+    </div>`;
+}
 
-let jsView = new EditorView({
-  doc: "console.log('hello world!)\n",
-  parent: edJS,
-  extensions: [editorSetup, javascript()],
-});
+function renderView() {
+  render(view(), document.body);
+}
 
-resizeHandler(workspace);
+function init() {
+  renderView();
 
-// resizeHandler("split-resize");
-// resizeHandler("title-css");
-// resizeHandler("title-js");
+  Split(["#editor-html", "#editor-css", "#editor-js"], {
+    cursor: "row-resize",
+    minSize: 0,
+    direction: "vertical",
+    gutterSize: 7,
+  });
+
+  Split(["#editor-container", "#output"], { gutterSize: 7 });
+
+  cssView = new EditorView({
+    parent: document.getElementById("css-container"),
+    state: cssState("h1 {font-family: monospace;}"),
+  });
+
+  htmlView = new EditorView({
+    parent: document.getElementById("html-container"),
+    state: htmlState("<h1>hello world</h1>"),
+  });
+
+  jsView = new EditorView({
+    parent: document.getElementById("js-container"),
+    state: jsState("console.log('hello world');"),
+  });
+
+  setEditorStates();
+  renderView();
+}
+
+init();
